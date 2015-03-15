c4ec17153037ca58c0661434a2bcdd76976038a3
Javier Lurgo (ntbk) <jlurgo@gmail.com>
2015-03-15 20:12:22 -0300

    commit inicial

---
+++ Main.js
@@ -0,0 +1,92 @@
+var onDeviceReady = function() {  
+	DivConsola.start();
+	console.log("Arrancando..");
+//	vx.start({verbose:true});
+//    
+//    vx.conectarPorWebSockets({
+//        url:'https://router-vortex.herokuapp.com'
+//    }); 
+	var errorCallback = function(message) {
+		console.log('Error: ' + message);
+	};
+	
+	var abrirPuertoSerie = function(){
+		serial.open ({baudRate: 57600},
+			function(successMessage) {
+				console.log("puerto serie abierto:", successMessage);
+				$("#btn_enviar").bind('touchstart', function(){
+					serial.write(
+						JSON.stringify({
+							estadoBoton:"presionado"
+						}),
+						function(successMessage) {
+							console.log(successMessage);
+						},
+						function(err){
+							console.log("error al enviar por puerto serie:", err);
+						}
+					);	
+				});
+
+				$("#btn_enviar").bind('touchend', function(){
+					serial.write(
+						JSON.stringify({
+							estadoBoton:"suelto"
+						}),
+						function(successMessage) {
+							console.log(successMessage);
+						},
+						function(err){
+							console.log("error al enviar por puerto serie:", err);
+						}
+					);	
+				});
+				
+				serial.registerReadCallback(
+					function(data){
+						var view = new Uint8Array(data);
+						console.log("recibido por puerto serie:", view);
+					},
+					function(err){
+						console.log("error al registrar callback:", err);
+					}
+				);
+			},
+			function(err){
+				console.log("error al abrir puerto serie:", err);
+			}
+		);
+	};
+	
+	serial.requestPermission(//{vid: '0x2341', pid: '0x0001'}, 
+							 function(successMessage) {
+			console.log("permiso concedido para usar puerto serie:", successMessage);
+			serial.close(function(){
+				console.log("puerto serie cerrado");
+				abrirPuertoSerie();
+			}, function(err){
+				console.log("error al cerrar puerto serie");
+				abrirPuertoSerie();
+			});
+		},
+		function(err){
+			console.log("error al pedir permiso para usar puerto serie:", err);
+		}
+	);
+};
+
+$(document).ready(function() {  
+    //toda esta garcha es para detectar si la aplicacion esta corriendo en un celular o en una pc.
+    //En el celular para arrancar la app hay que esperar al evento deviceReady, en la pc solo al documentReady
+    window.isphone = false;
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        window.isphone = true;
+    }
+
+    if(window.isphone) {
+        document.addEventListener("deviceready", onDeviceReady, false);
+    } else {
+        onDeviceReady();
+    }
+});
+
---
+++ PruebaUsbSerieBidireccional_Arduino/PruebaUsbSerieBidireccional_Arduino.ino
@@ -0,0 +1,68 @@
+#include <aJSON.h>
+
+#define SIZE_BUFFER_ENTRADA 256
+
+const int buttonPin = 2;     // the number of the pushbutton pin
+const int ledPin =  3;      // the number of the LED pin
+int estadoAnteriorBoton = 0;
+int estadoActualBoton = 0;
+
+const char* estados_boton[] = { "presionado", "suelto"};
+
+char buffer_entrada[256];
+int ultimo_byte_entrada = 0;
+
+void setup(void)
+{ 
+  	pinMode(ledPin, OUTPUT);      
+  	pinMode(buttonPin, INPUT);     
+
+  	Serial.begin(115200);
+	blanquearBufferEntrada();
+	
+}
+
+void blanquearBufferEntrada(void){
+	for(int i=0; i<SIZE_BUFFER_ENTRADA; i++){
+		buffer_entrada[i] = '\0';	
+	}	
+}
+
+void loop(void)
+{
+	// read the state of the pushbutton value:
+	estadoActualBoton = digitalRead(buttonPin);
+
+	if(estadoActualBoton != estadoAnteriorBoton)
+	{
+		String mensaje = "";
+		mensaje = mensaje + "{\"estadoBoton\":\"" + estados_boton[estadoActualBoton] + "\"}";
+		Serial.println(mensaje);
+	}
+	estadoAnteriorBoton = estadoActualBoton;
+}
+
+void serialEvent() {
+	while (Serial.available()) {  
+		char byte_leido = Serial.read();
+		if(byte_leido == '\n' || byte_leido == '\r'){
+			aJsonObject *msg = aJson.parse(buffer_entrada);	
+			if (msg) {
+				aJsonObject *estadoBoton = aJson.getObjectItem(msg, "estadoBoton");
+				if (estadoBoton) {
+					if(strcmp(estadoBoton->valuestring, "presionado")==0)digitalWrite(ledPin, HIGH);
+					if(strcmp(estadoBoton->valuestring, "suelto")==0)digitalWrite(ledPin, LOW);
+				}else{
+					Serial.println("el mensaje no tiene estadoBoton");
+				}				
+			}else{
+				Serial.println("error al interpretar json");
+			}
+			blanquearBufferEntrada();
+			ultimo_byte_entrada = 0;
+		}else{      
+			buffer_entrada[ultimo_byte_entrada] = byte_leido;	
+			ultimo_byte_entrada++;
+		}
+	}
+}
